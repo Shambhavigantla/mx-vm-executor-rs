b50dacb33195801db905a9d10a443344d46ba58f
Shambhavi <dharitri@dharitri.org>
2026-05-14 16:14:24 +0530

    new

--- vm-executor-wasmer/src/wasmer_imports.rs
+++ vm-executor-wasmer/src/wasmer_imports.rs
@@ -6,7 +6,7 @@
 
 #![allow(clippy::too_many_arguments)]
 
-use wasmer::{Function, ImportObject, Store, imports};
+use wasmer::{imports, Function, ImportObject, Store};
 
 use crate::wasmer_vm_hooks::VMHooksWrapper;
 
@@ -651,13 +651,13 @@ fn wasmer_import_managed_is_builtin_function(env: &VMHooksWrapper, function_name
 }
 
 #[rustfmt::skip]
-fn wasmer_import_managed_drwa_sync_mirror(env: &VMHooksWrapper, payload_handle: i32) -> i32 {
-    env.vm_hooks.managed_drwa_sync_mirror(payload_handle)
+fn wasmer_import_managed_drwasync_mirror(env: &VMHooksWrapper, payload_handle: i32) -> i32 {
+    env.vm_hooks.managed_drwasync_mirror(payload_handle)
 }
 
 #[rustfmt::skip]
-fn wasmer_import_managed_drwa_native_governance_query(env: &VMHooksWrapper, query_type: i32, key_handle: i32, dest_handle: i32) -> i32 {
-    env.vm_hooks.managed_drwa_native_governance_query(query_type, key_handle, dest_handle)
+fn wasmer_import_managed_drwanative_governance_query(env: &VMHooksWrapper, query_type: i32, key_handle: i32, dest_handle: i32) -> i32 {
+    env.vm_hooks.managed_drwanative_governance_query(query_type, key_handle, dest_handle)
 }
 
 #[rustfmt::skip]
@@ -1405,66 +1405,6 @@ fn wasmer_import_managed_verify_blsaggregated_signature(env: &VMHooksWrapper, ke
     env.vm_hooks.managed_verify_blsaggregated_signature(key_handle, message_handle, sig_handle)
 }
 
-#[rustfmt::skip]
-fn wasmer_import_activate_unsafe_mode(env: &VMHooksWrapper) {
-    env.vm_hooks.activate_unsafe_mode()
-}
-
-#[rustfmt::skip]
-fn wasmer_import_deactivate_unsafe_mode(env: &VMHooksWrapper) {
-    env.vm_hooks.deactivate_unsafe_mode()
-}
-
-#[rustfmt::skip]
-fn wasmer_import_managed_get_num_errors(env: &VMHooksWrapper) -> i32 {
-    env.vm_hooks.managed_get_num_errors()
-}
-
-#[rustfmt::skip]
-fn wasmer_import_managed_get_error_with_index(env: &VMHooksWrapper, index: i32, error_handle: i32) {
-    env.vm_hooks.managed_get_error_with_index(index, error_handle)
-}
-
-#[rustfmt::skip]
-fn wasmer_import_managed_get_last_error(env: &VMHooksWrapper, error_handle: i32) {
-    env.vm_hooks.managed_get_last_error(error_handle)
-}
-
-#[rustfmt::skip]
-fn wasmer_import_managed_verify_groth16(env: &VMHooksWrapper, curve_id: i32, proof_handle: i32, vk_handle: i32, pub_witness_handle: i32) -> i32 {
-    env.vm_hooks.managed_verify_groth16(curve_id, proof_handle, vk_handle, pub_witness_handle)
-}
-
-#[rustfmt::skip]
-fn wasmer_import_managed_verify_plonk(env: &VMHooksWrapper, curve_id: i32, proof_handle: i32, vk_handle: i32, pub_witness_handle: i32) -> i32 {
-    env.vm_hooks.managed_verify_plonk(curve_id, proof_handle, vk_handle, pub_witness_handle)
-}
-
-#[rustfmt::skip]
-fn wasmer_import_managed_add_ec(env: &VMHooksWrapper, curve_id: i32, group_id: i32, point1_handle: i32, point2_handle: i32, result_handle: i32) -> i32 {
-    env.vm_hooks.managed_add_ec(curve_id, group_id, point1_handle, point2_handle, result_handle)
-}
-
-#[rustfmt::skip]
-fn wasmer_import_managed_mul_ec(env: &VMHooksWrapper, curve_id: i32, group_id: i32, point_handle: i32, scalar_handle: i32, result_handle: i32) -> i32 {
-    env.vm_hooks.managed_mul_ec(curve_id, group_id, point_handle, scalar_handle, result_handle)
-}
-
-#[rustfmt::skip]
-fn wasmer_import_managed_multi_exp_ec(env: &VMHooksWrapper, curve_id: i32, group_id: i32, points_handle: i32, scalars_handle: i32, result_handle: i32) -> i32 {
-    env.vm_hooks.managed_multi_exp_ec(curve_id, group_id, points_handle, scalars_handle, result_handle)
-}
-
-#[rustfmt::skip]
-fn wasmer_import_managed_map_to_curve_ec(env: &VMHooksWrapper, curve_id: i32, group_id: i32, element_handle: i32, result_handle: i32) -> i32 {
-    env.vm_hooks.managed_map_to_curve_ec(curve_id, group_id, element_handle, result_handle)
-}
-
-#[rustfmt::skip]
-fn wasmer_import_managed_pairing_checks_ec(env: &VMHooksWrapper, curve_id: i32, points_g1_handle: i32, points_g2_handle: i32) -> i32 {
-    env.vm_hooks.managed_pairing_checks_ec(curve_id, points_g1_handle, points_g2_handle)
-}
-
 pub fn generate_import_object(store: &Store, env: &VMHooksWrapper) -> ImportObject {
     imports! {
         "env" => {
@@ -1596,8 +1536,8 @@ pub fn generate_import_object(store: &Store, env: &VMHooksWrapper) -> ImportObje
             "managedGetCodeMetadata" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_get_code_metadata),
             "managedGetCodeHash" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_get_code_hash),
             "managedIsBuiltinFunction" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_is_builtin_function),
-            "managedDRWASyncMirror" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_drwa_sync_mirror),
-            "managedDRWANativeGovernanceQuery" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_drwa_native_governance_query),
+            "managedDRWASyncMirror" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_drwasync_mirror),
+            "managedDRWANativeGovernanceQuery" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_drwanative_governance_query),
             "bigFloatNewFromParts" => Function::new_native_with_env(store, env.clone(), wasmer_import_big_float_new_from_parts),
             "bigFloatNewFromFrac" => Function::new_native_with_env(store, env.clone(), wasmer_import_big_float_new_from_frac),
             "bigFloatNewFromSci" => Function::new_native_with_env(store, env.clone(), wasmer_import_big_float_new_from_sci),
@@ -1747,18 +1687,6 @@ pub fn generate_import_object(store: &Store, env: &VMHooksWrapper) -> ImportObje
             "managedVerifySecp256r1" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_verify_secp256r1),
             "managedVerifyBLSSignatureShare" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_verify_blssignature_share),
             "managedVerifyBLSAggregatedSignature" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_verify_blsaggregated_signature),
-            "activateUnsafeMode" => Function::new_native_with_env(store, env.clone(), wasmer_import_activate_unsafe_mode),
-            "deactivateUnsafeMode" => Function::new_native_with_env(store, env.clone(), wasmer_import_deactivate_unsafe_mode),
-            "managedGetNumErrors" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_get_num_errors),
-            "managedGetErrorWithIndex" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_get_error_with_index),
-            "managedGetLastError" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_get_last_error),
-            "managedVerifyGroth16" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_verify_groth16),
-            "managedVerifyPlonk" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_verify_plonk),
-            "managedAddEC" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_add_ec),
-            "managedMulEC" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_mul_ec),
-            "managedMultiExpEC" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_multi_exp_ec),
-            "managedMapToCurveEC" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_map_to_curve_ec),
-            "managedPairingChecksEC" => Function::new_native_with_env(store, env.clone(), wasmer_import_managed_pairing_checks_ec),
 
         }
     }
